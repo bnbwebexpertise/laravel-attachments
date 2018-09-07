cb535a03f3e4bc5bc645774b38b186ce8308771b
Vongola <vongola12324@coder.tw>
2018-09-07 20:37:29 +0800

    Add traditional chinese (zh-tw) traslation

---
+++ translations/zh-tw/messages.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'errors' => [
+        'access_denied' => '拒絕存取。',
+        'not_found' => '查無檔案。',
+        'expired' => '檔案已不可存取。',
+        'upload_failed' => '上傳失敗。',
+        'upload_denied' => '上傳被拒。',
+        'delete_denied' => '刪除被拒。',
+        'delete_failed' => '刪除失敗。',
+    ],
+    'console' => [
+        'done' => '完成！',
+        'cleanup_description' => '移除未綁定到模型實體的附件。',
+        'cleanup_confirm' => '確定要刪除未綁定的附件嗎？',
+        'cleanup_option_since' => '附件最短保留時間（從修改時間開始算，單位：分鐘）。',
+        'cleanup_no_data' => '沒有附件可以被操作。',
+    ],
+];
